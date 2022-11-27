f1c372eb28eeec2467d81de729fcb496bd64d1e0
Toxicwaste613 <lucas.mercure@hotmail.com>
2022-11-27 15:49:25 -0500

    first commit

---
+++ wwwroot/ImagesManager2022/js/api.js
@@ -0,0 +1,86 @@
+const apiBaseURL = "http://localhost:5000/";
+const userBaseURL = "http://localhost:5000/accounts/";
+
+
+function HEAD(successCallBack, errorCallBack) {
+    $.ajax({
+        url: apiBaseURL,
+        type: 'HEAD',
+        contentType: 'text/plain',
+        complete: request => { successCallBack(request.getResponseHeader('ETag')) },
+        error: function (jqXHR) { errorCallBack(jqXHR.status) }
+    });
+}
+function GET_ID(id, successCallBack, errorCallBack) {
+    $.ajax({
+        url: apiBaseURL + "/" + id,
+        type: 'GET',
+        success: data => { successCallBack(data); },
+        error: function (jqXHR) { errorCallBack(jqXHR.status) }
+    });
+}
+function GET_ALL(successCallBack, errorCallBack, queryString = null) {
+    let url = apiBaseURL + "api/images" + (queryString ? queryString : "");
+    $.ajax({
+        url: url,
+        type: 'GET',
+        success: (data, status, xhr) => { successCallBack(data, xhr.getResponseHeader("ETag")) },
+        error: function (jqXHR) { errorCallBack(jqXHR.status) }
+    });
+}
+function PostImage(data, successCallBack, errorCallBack) {
+    $.ajax({
+        url: apiBaseURL,
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify(data),
+        success: (data) => { successCallBack(data) },
+        error: function (jqXHR) { errorCallBack(jqXHR.status) }
+    });
+}
+function PutImage(bookmark, successCallBack, errorCallBack) {
+    $.ajax({
+        url: apiBaseURL + "/" + bookmark.Id,
+        type: 'PUT',
+        contentType: 'application/json',
+        data: JSON.stringify(bookmark),
+        success: () => { successCallBack() },
+        error: function (jqXHR) { errorCallBack(jqXHR.status) }
+    });
+}
+function DeleteImage(id, successCallBack, errorCallBack) {
+    $.ajax({
+        url: apiBaseURL + "/" + id,
+        type: 'DELETE',
+        success: () => { successCallBack() },
+        error: function (jqXHR) { errorCallBack(jqXHR.status) }
+    });
+}
+function PostUser(data, successCallBack, errorCallBack) {
+    $.ajax({
+        url: apiBaseURL,
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify(data),
+        success: (data) => { successCallBack(data) },
+        error: function (jqXHR) { errorCallBack(jqXHR.status) }
+    });
+}
+function PutUser(bookmark, successCallBack, errorCallBack) {
+    $.ajax({
+        url: apiBaseURL + "/" + bookmark.Id,
+        type: 'PUT',
+        contentType: 'application/json',
+        data: JSON.stringify(bookmark),
+        success: () => { successCallBack() },
+        error: function (jqXHR) { errorCallBack(jqXHR.status) }
+    });
+}
+function DeleteUser(id, successCallBack, errorCallBack) {
+    $.ajax({
+        url: apiBaseURL + "/" + id,
+        type: 'DELETE',
+        success: () => { successCallBack() },
+        error: function (jqXHR) { errorCallBack(jqXHR.status) }
+    });
+}
